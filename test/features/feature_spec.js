a1d099d55124de19837abe8df9ccc96c5da78625
timchipperfield <timchipperfield@gmail.com>
2016-07-27 17:33:21 +0100

    improved feature test code

--- test/features/feature_spec.js
+++ test/features/feature_spec.js
@@ -1,14 +1,27 @@
 // var casper = require('casper').create();
+//define ip and port to web service
 
-casper.test.begin("index page", function(test) {
+
+casper.test.begin("index page", 2, function suite(test) {
+
   casper.start("http://localhost:8000/ios/www/index.html", function() {
+
     test.assertHttpStatus(200);
     casper.then(function() {
-      this.echo('First page:' + this.getTitle());
+        this.echo('First page:' + this.getTitle());
     });
+
+    test.assertEvalEquals(function() {
+        return __utils__.findOne('h1').textContent;
+      }, 'Chasing Shadows');
+
   });
+
+
+
+
+casper.run(function() {
+  test.done();
 });
 
-
-
-casper.run();
+});
