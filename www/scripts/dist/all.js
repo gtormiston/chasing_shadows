b24e95853b2355a7d4dad9a0e214bda189e07ece
Graham Ormiston <grahamormiston@gmail.com>
2016-08-03 09:42:15 +0100

    Temporary attack page layout

--- www/scripts/dist/all.js
+++ www/scripts/dist/all.js
@@ -1020,6 +1020,7 @@ $(document).ready(function() {
   $("#google_map").css("height", $(document).height());
   $("#form").css("height", $(document).height());
   $("#welcome_page").css("height", $(document).height());
+  $(".attack").css("height", $(document).height());
 
 });
 
--- www/scripts/js/match_height.js
+++ www/scripts/js/match_height.js
@@ -3,5 +3,6 @@ $(document).ready(function() {
   $("#google_map").css("height", $(document).height());
   $("#form").css("height", $(document).height());
   $("#welcome_page").css("height", $(document).height());
+  $(".attack").css("height", $(document).height());
 
 });
